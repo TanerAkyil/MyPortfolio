c59f753b47cdd607b20721550bbc994a676df93d
Taner Akyil <tano_1907@hotmail.com>
2019-02-20 09:49:23 +0300

    Add project files.

---
+++ Ali.Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Ali.Data.Builder;
+using Ali.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ali.Data
+{
+    public class ApplicationDbContext :IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    
+
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            new PostBuilder(modelBuilder.Entity<Post>());
+            new CategoryBuilder(modelBuilder.Entity<Category>());
+        }
+    }
+}
